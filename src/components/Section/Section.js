223f83925152844be88283bac0aa7519267038e1
Kateryna Karpova <katyakarpova@gmail.com>
2021-09-01 18:28:15 +0200

    Правки css

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 function Section({ title, children }) {
   return (
-    <div>
+    <div className={s.container}>
       {title && <h2 className={s.title}>{title}</h2>}
       {children}
     </div>
